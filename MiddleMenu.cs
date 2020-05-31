fdb654f2ec62aaf39485615c5132e0acdf66d161
Markovic55 <50618578+Markovic55@users.noreply.github.com>
2020-05-31 21:12:16 +0200

    Add files via upload

---
+++ MiddleMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MiddleMenu : MonoBehaviour
+{
+    public Text playerdisplay;
+    private void Start()
+    {
+        if (DBManager.LoggedIn)
+        {
+            playerdisplay.text = "Player: " + DBManager.username;
+        }
+    }
+
+    public void GoToGame()
+    
+        {
+            SceneManager.LoadScene(4);
+        }
+
+    public void GoToScore()
+
+    {
+        SceneManager.LoadScene(5);
+    }
+
+
+
+}
